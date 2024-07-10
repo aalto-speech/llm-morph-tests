825b4328bdaadf21f9133a3bcb4876328a21c198
Anssi Moisio <anssi.moisio@aalto.fi>
2024-07-10 14:49:28 +0300

    deleted files not relevant for the CMCL paper

--- get_plain_words.py
+++
@@ -1,8 +0,0 @@
-
-
-with open('data/omorfi_noun_lexemes_filtered_inflected.txt.00', 'r', encoding='utf-8') as f:
-    lines = [l.split(':')[0].strip() for l in  f.readlines() if l.strip()]
-
-with open('data/omorfi_noun_lexemes_filtered_inflected.txt.00.plain', 'w', encoding='utf-8') as f:
-    f.write('\n'.join(lines))
-    f.write('\n')
--- slrm-freq-stats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=freq_stats
-#SBATCH --time=00:30:00
-#SBATCH --mem=2G
-#SBATCH --output=log/%x_%j.out
-
-args=("$@")
-python freq_stats_from_conllu.py ${args[@]}
